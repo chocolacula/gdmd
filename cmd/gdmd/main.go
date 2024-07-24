a886a937d95cf99743c25bf8283ae360512fc215
Maxim Voloshin <max.voloshin@pepperstone.com>
2024-07-24 21:52:11 +0300

    improve help menu and error handling

--- cmd/gdmd/main.go
+++ cmd/gdmd/main.go
@@ -7,9 +7,20 @@ import (
 	"path/filepath"
 )
 
-const version = "0.0.0"
+const (
+	version = "0.0.0"
+	usage   = `usage: gdmd <directory>
+
+go doc markdown
+
+options:`
+)
 
 func main() {
+	flag.Usage = func() {
+		println(usage)
+		flag.PrintDefaults()
+	}
 	vFlag := flag.Bool("v", false, "print version")
 
 	flag.Parse()
@@ -21,6 +32,15 @@ func main() {
 
 	args := os.Args
 	root, _ := filepath.Abs(args[1])
+
+	_, err := os.Stat(root)
+	if err != nil {
+		if os.IsNotExist(err) {
+			log.Fatalf("directory %s does not exist", root)
+		} else {
+			log.Fatal(err)
+		}
+	}
 
 	pkg, err := Parse(root, "")
 	if err != nil {
